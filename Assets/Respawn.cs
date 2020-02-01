e23533db1e70173ea0d8d5eca5a673e422df3c8c
cotsioslord <60516663+cotsioslord@users.noreply.github.com>
2020-02-01 21:55:33 +0200

    Respawn
    Fixing respawn

---
+++ Assets/Respawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform player;
+    public Transform respawnPoint;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = respawnPoint.transform.position;
+    }
+    
+}
