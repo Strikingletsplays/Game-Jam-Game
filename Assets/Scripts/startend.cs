187847649da7427f9529a6366569a000ade7e69e
VirusActive <58213929+VirusActive@users.noreply.github.com>
2020-02-02 15:58:35 +0200

    menu audio
    asdad

---
+++ Assets/Scripts/startend.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startend : MonoBehaviour
+{
+
+	public void Onstart()
+	{ 
+print(SceneManager.GetActiveScene().buildIndex);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+}
+  	
+public void QuitGame () 
+{ // your quitting function
+		Application.Quit(); // command to quit application
+	}
+	}
